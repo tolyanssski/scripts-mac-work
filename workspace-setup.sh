ffac703337baec7840e6c3c39c7cdb8965be0c2f
Anatoly <a.grigoriev@glexglobal.com>
2025-10-27 09:24:47 +0000

    11

--- workspace-setup.sh
+++ workspace-setup.sh
@@ -4,3 +4,5 @@
 echo 'net.ipv4.ip_unprivileged_port_start=0' | sudo tee /etc/sysctl.d/60-unprivileged-ports.conf
 echo 'net.ipv6.ip_unprivileged_port_start=0' | sudo tee -a /etc/sysctl.d/60-unprivileged-ports.conf
 sudo sysctl --system
+
+git config --global --add url."git@ssh.gitlab.devsmart.ru:".insteadOf "https://gitlab.devsmart.ru/"
